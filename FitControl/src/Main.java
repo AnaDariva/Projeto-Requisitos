27b21f6575b59f595fb5c1e00e0a58fd723933be
Ana Luísa Dariva <anadarivaramos@gmail.com>
2025-06-19 21:34:02 -0300

    Update Main.java

--- FitControl/src/Main.java
+++ FitControl/src/Main.java
@@ -1,7 +1,7 @@
 import service.AulaService;
 import service.PagamentoService;
 import service.AlunoService;
-import service.RelatorioService;  // Certifique-se de importar o serviço de relatórios
+import service.RelatorioService;  
 import model.Aula;
 import model.Pagamento;
 import model.Aluno;
